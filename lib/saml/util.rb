1824b45d290ca1bfd71233231305ab561a775bdb
pnabage <pnabage@gmail.com>
2017-10-25 14:28:12 +0530

    sso fix

--- lib/saml/util.rb
+++ lib/saml/util.rb
@@ -40,6 +40,7 @@ module Saml
 
       def sign_xml(message, format = :xml, &block)
         message.add_signature
+	message.assertions[0].issuer = message.assertions[0].issuer.sub(/_idp$/,'') if message.assertions
         document = Xmldsig::SignedDocument.new(message.send("to_#{format}"))
         #changing issuer to entity id instead of internal provider store id
         document.document.at_xpath("/samlp:Response/saml:Issuer/text()").content = message.issuer.sub(/_idp$/,'')
